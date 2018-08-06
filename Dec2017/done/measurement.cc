5b16ea844197603b54f3ed1457e7e6cbed18052e
Wendy Zhang <wendyzhang250@gmail.com>
2018-08-05 22:45:59 -0700

    relocated silver stuff

--- Dec2017/done/measurement.cc
+++ Dec2017/done/measurement.cc
@@ -7,59 +7,70 @@ LANG: C++
 #include <iostream>
 #include <fstream>
 #include <string>
-
+#include <algorithm>
+#include <list> 
+#include <map> 
+#include <ctime>
+#define MAX 100001
+#define SVAL 0 
 using namespace std;
 
-//Bessie = 0, Elsie = 1, and Mildred = 2 
+// if the biggest value --> 
+bool testing = true, testing2 = true;
 
-int output[3][101];
+struct cow{
+    int day, id, milk;
+};
 
-
-int main() {
-    ifstream fin ("measurement.in");
-    ofstream fout("measurement.out");
-    //cout << "Bessie = 0, Elsie = 1, and Mildred = 2\n";  
-    int N, max = 0, daysChanged = 0;
-    fin >> N;
-    for(int i = 0, id, day, delta; i < N; i++){
-        string stmp;
-        fin >> day >> stmp >> delta;
-        //cout << day << " " << stmp << " " << delta << "\t";
-        if(stmp == "Bessie"){ id = 0; }
-        else if(stmp == "Elsie") { id = 1; }
-        else if(stmp == "Mildred") { id = 2; }
-        else {cout << "different name?: " << stmp << endl;}
-        output[id][day] += delta;
-        //cout << id << "\n";
-        if(day > max){ max = day;}
-    }
-    output[0][0] = 7,  output[1][0] = 7,  output[2][0] = 7;
-    //calculate actual max
-    cout << max << endl;
-    bool cowIDs[3] = {true, true, true};
-    for(int i = 1; i <= max; i++){
-        int maxOut = 0;
-        for(int j = 0; j < 3; j++){
-            output[j][i] =  output[j][i] +  output[j][i-1];
-            if(output[j][i] > maxOut) {maxOut = output[j][i];}
-        }
-        bool changed = false;
-        for(int j = 0; j < 3; j++){
-            if(output[j][i] == maxOut && !cowIDs[j]){
-                cowIDs[j] = true, changed = true;
-                //cout << i << ": " << j << " added\n";
-            } else if(output[j][i] < maxOut && cowIDs[j]){
-                cowIDs[j] = false, changed = true;
-                //cout << i << ": " << j << " taken down\n";
-            }
-        }
-        if(changed) {daysChanged++;}
-    }
-    fout << daysChanged << endl;
-    return 0;
+bool compCow(cow a, cow b){
+    return a.day < b.day;
 }
 
 
+void print(cow arr[], int N);
+
+int N, G, daysChanged = 0, maxMilk = SVAL;
+map<int, int, greater<int> > cc;
+map<int, int> cowlink_log;
+bool old_top, is_last, is_only, new_top; 
+cow log[MAX];
+
+int main() {
+    ifstream fin ("measurement.in");
+    ofstream fout("measurement.out"); 
+    
+    fin >> N >> G;
+    for(int i = 0; i < N; i++){
+        fin >> log[i].day  >> log[i].id >> log[i].milk;
+        cowlink_log[log[i].id] = SVAL;
+    }
+    sort(log, log+N, compCow);
+    //print(log, N);
+    cc[SVAL] = N+1; //starts with this #
+    for(int i = 0; i < N; i++){
+        cow curr = log[i];
+        old_top = (cc.begin() -> first == cowlink_log[curr.id]); 
+        cc[cowlink_log[curr.id]]--;
+        is_last = (!cc[cowlink_log[curr.id]]);
+        if(is_last){    cc.erase(cowlink_log[curr.id]);     }
+        cowlink_log[curr.id] += curr.milk;
+        cc[cowlink_log[curr.id]]++;
+        is_only = (cc.begin() -> second == 1);
+        new_top = (cc.begin() -> first == cowlink_log[curr.id]);
+        if((old_top && (!new_top || !is_only || !is_last)) || (!old_top && new_top)){
+            daysChanged++;
+        }
+    }
+    fout << daysChanged << endl;
+    cout << daysChanged << endl;
+    return 0;
+}
+
+void print(cow arr[], int N){
+    for(int i = 0; i < N; i++){
+        cout << arr[i].day << " " << arr[i].id << " " << arr[i].milk << endl;
+    }
+}
 
 
 
--- Dec2017/done/shuffle.cc
+++ Dec2017/done/shuffle.cc
@@ -1,50 +1,81 @@
 /*
 ID: wenxzha1 
-TASK: shuffle
+TASK: shuffle 
 LANG: C++                 
 */
 /* LANG can be C++11 or C++14 for those more recent releases */
 #include <iostream>
 #include <fstream>
+#include <cstring>
 #include <string>
+#include <queue>
+#define MAX 100001
 
 using namespace std;
 
-int id_curr[101];
-int id_prev[101];
-int order[101];
-int N;
-
-void print(int arr[]){
+void print(int arr[], int N){
     for(int i = 1; i <= N; i++){
-        cout << arr[i] << endl;
+        cout << arr[i] << " ";
     }
-    cout << endl;
+    cout << endl << endl;
+}
+void print(bool arr[], int N){
+    for(int i = 1; i <= N; i++){
+        cout << arr[i] << " ";
+    }
+    cout << endl << endl;
 }
 
 int main() {
     ifstream fin ("shuffle.in");
     ofstream fout("shuffle.out");
-    fin >> N; 
+    
+    int N;
+    fin >> N;
+    int cnt = N;
+    int order[N+1];
+    int back_cnt[N+1];
+    queue <int> q;
+    
+    //memset(test, 0, sizeof(test));
+    memset(back_cnt, 0, sizeof(back_cnt));
+
+    //back_cnt[][0] = first value && back_cnt[][1] = last value
     for(int i = 1; i <= N; i++){
         fin >> order[i];
+        back_cnt[order[i]]++;
     }
-    for(int i = 1; i <= N; i++){
-        fin >> id_prev[i];
-    }
-    //print(id_prev);
-    for(int j = 1; j <= 3; j++){
-        for(int i = 1; i <= N; i++) { 
-            id_curr[i] = id_prev[order[i]];
-        }
-        for(int i = 1; i <= N; i++) {
-            id_prev[i] = id_curr[i];
-        }
-        //print(id_prev);
-    }
-    for(int i = 1; i <= N; i++) {
-        fout << id_prev[i] << endl;
-    }
+    //print(back_cnt, N);  
+    //memset(flag, 0, sizeof(flag));
 
+    for(int i = 1; i <= N; i++){
+        if(!back_cnt[i]){
+            //cout << i << endl;
+            q.push(i);
+            cnt--;
+        }
+    }
+    
+    while(!q.empty()){
+       int i = q.front();
+       q.pop();
+       //back_cnt[order[i]]--;
+       if(!(--back_cnt[order[i]])){
+           q.push(order[i]);
+           cnt--;
+       }
+       //print(back_cnt, N);
+    }
+    
+    //cout << cnt << endl;
+    fout << cnt << endl;
+    //print(test, N);
     return 0;
 }
+
+
+
+
+
+
+
